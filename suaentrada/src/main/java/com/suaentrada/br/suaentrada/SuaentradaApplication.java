9fa76d5d96e25ab6ac6e329b8890deabf59324b2
RochaGR <gutop.rocha@gmail.com>
2025-09-10 14:38:16 -0300

    criação do projeto

---
+++ suaentrada/src/main/java/com/suaentrada/br/suaentrada/SuaentradaApplication.java
@@ -0,0 +1,13 @@
+package com.suaentrada.br.suaentrada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuaentradaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuaentradaApplication.class, args);
+	}
+
+}
---
+++ suaentrada/src/test/java/com/suaentrada/br/suaentrada/SuaentradaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suaentrada.br.suaentrada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuaentradaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
