8363504b3ae3d3a74057a4492c6e38b387af9c66
RochaGR <gutop.rocha@gmail.com>
2025-09-10 16:40:15 -0300

    Feat: Criação dos models, evento,inscrição, usuario

--- suaentrada/src/main/java/com/suaentrada/br/suaentrada/model/Inscricao.java
+++ suaentrada/src/main/java/com/suaentrada/br/suaentrada/model/Inscricao.java
@@ -1,4 +1,39 @@
 package com.suaentrada.br.suaentrada.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Inscricao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "CDINSCRICAO")
+    private Long codigoInscricao;
+
+    @Column(name = "DTINSCRICAO")
+    private LocalDate dataInscricao;
+
+    @Column(name = "HRINSCRICAO")
+    private LocalTime horaInscricao;
+
+    @ManyToOne
+    @JoinColumn(name = "CDUSUARIO")
+    private Usuario usuario;
+
+    @ManyToOne
+    @JoinColumn(name = "CDEVENTO")
+    private Evento evento;
+
 }
--- suaentrada/src/main/java/com/suaentrada/br/suaentrada/model/Usuario.java
+++ suaentrada/src/main/java/com/suaentrada/br/suaentrada/model/Usuario.java
@@ -1,6 +1,40 @@
 package com.suaentrada.br.suaentrada.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "CDUSUARIO")
+    private Long codigoUsuario ;
+
+    @Column(name = "NMUSUARIO")
+    private String nomeUsuario;
+
+    @Column(name = "NMEMAIL")
+    private String emailUsuario;
+
+    @Column(name = "NMSENHA")
+    private String senhaUsuario;
+
+    @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Inscricao> inscricoes = new ArrayList<>();
+
 }
+
+
