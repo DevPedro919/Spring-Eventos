76571566691b4c1b504d917ba0d2cef62baa0f78
RochaGR <gutop.rocha@gmail.com>
2025-09-11 17:19:15 -0300

    Feat: Criação EventoMapper

--- suaentrada/src/main/java/com/suaentrada/br/suaentrada/dto/mapper/EventoMapper.java
+++ suaentrada/src/main/java/com/suaentrada/br/suaentrada/dto/mapper/EventoMapper.java
@@ -1,13 +1,14 @@
 package com.suaentrada.br.suaentrada.dto.mapper;
 
 import com.suaentrada.br.suaentrada.dto.EventoDto;
-import com.suaentrada.br.suaentrada.model.Evento;
+import com.suaentrada.br.suaentrada.model.EventoModel;
 
 public class EventoMapper {
 
-    public static EventoDto toEventoDto(Evento evento){
-        if(evento == null) return null;
+    public static EventoDto toDto(EventoModel evento) {
+        if (evento == null) return null;
         return new EventoDto(
+                evento.getCodigoEvento(),
                 evento.getTituloEvento(),
                 evento.getDescricaoEvento(),
                 evento.getDataEvento(),
@@ -18,7 +19,17 @@ public class EventoMapper {
         );
     }
 
-        
-
+    public static EventoModel toEntity(EventoDto dto) {
+        if (dto == null) return null;
+        EventoModel evento = new EventoModel();
+        evento.setTituloEvento(dto.tituloEvento());
+        evento.setDescricaoEvento(dto.descricaoEvento());
+        evento.setDataEvento(dto.dataEvento());
+        evento.setHoraEvento(dto.horaEvento());
+        evento.setLocalEvento(dto.localEvento());
+        evento.setVagasTotaisEvento(dto.vagasTotaisEvento());
+        evento.setVagasDisponiveisEvento(dto.vagasDisponiveisEvento());
+        return evento;
     }
 
+}
