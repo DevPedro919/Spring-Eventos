9e3596d984af6a7b28fcc0e297ed6e2bddfa62a0
RochaGR <gutop.rocha@gmail.com>
2025-09-11 17:19:59 -0300

    Feat: Criado Evento Service

--- suaentrada/src/main/java/com/suaentrada/br/suaentrada/service/EventoService.java
+++ suaentrada/src/main/java/com/suaentrada/br/suaentrada/service/EventoService.java
@@ -1,6 +1,39 @@
 package com.suaentrada.br.suaentrada.service;
 
+import com.suaentrada.br.suaentrada.dto.EventoDto;
+import com.suaentrada.br.suaentrada.dto.mapper.EventoMapper;
+import com.suaentrada.br.suaentrada.model.EventoModel;
+import com.suaentrada.br.suaentrada.repository.EventoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+@RequiredArgsConstructor
 public class EventoService {
 
-}
+    private final EventoRepository repository;
 
+    public EventoDto salvar(EventoDto dto) {
+        EventoModel evento = EventoMapper.toEntity(dto);
+        EventoModel salvo = repository.save(evento);
+        return EventoMapper.toDto(salvo);
+    }
+
+    public void excluir(Long id) {
+        repository.deleteById(id);
+    }
+
+    public EventoDto buscarPorId(Long id) {
+        return repository.findById(id).map(EventoMapper::toDto).orElse(null);
+    }
+
+    public List<EventoDto> buscarTodos() {
+        return repository.findAll().stream().map(EventoMapper::toDto).toList();
+    }
+
+
+
+}
