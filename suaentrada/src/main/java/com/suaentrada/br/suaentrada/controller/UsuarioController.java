0a97990e2b29a153fe218f9c68e599446f148af9
Victor Hugo De Pieri Justino <vdepierejustino@gmail.com>
2025-09-11 17:17:50 -0300

    Feat: Criação dos Controller e Service do usuario

--- suaentrada/src/main/java/com/suaentrada/br/suaentrada/controller/UsuarioController.java
+++ suaentrada/src/main/java/com/suaentrada/br/suaentrada/controller/UsuarioController.java
@@ -1,4 +1,35 @@
 package com.suaentrada.br.suaentrada.controller;
 
+import com.suaentrada.br.suaentrada.model.Usuario;
+import com.suaentrada.br.suaentrada.service.UsuarioService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/usuario")
+@RequiredArgsConstructor
 public class UsuarioController {
+    UsuarioService usuarioService;
+
+    @PostMapping
+    public Usuario salvar(@RequestBody Usuario usuario){
+        return usuarioService.salvar(usuario);
+    }
+
+    @GetMapping
+    public List<Usuario> listarTodos(){
+        return usuarioService.listarTodos();
+    }
+
+    @GetMapping("/{codigoUsuario}")
+    public Usuario buscar(@PathVariable Long codigoUsuario){
+        return usuarioService.buscarPorId(codigoUsuario);
+    }
+
+    @DeleteMapping("/{codigoUsuario")
+    public void deletar(@PathVariable Long codigoUsuario){
+        usuarioService.deletar(codigoUsuario);
+    }
 }
--- suaentrada/src/main/java/com/suaentrada/br/suaentrada/service/UsuarioService.java
+++ suaentrada/src/main/java/com/suaentrada/br/suaentrada/service/UsuarioService.java
@@ -1,4 +1,30 @@
 package com.suaentrada.br.suaentrada.service;
 
+import com.suaentrada.br.suaentrada.model.Usuario;
+import com.suaentrada.br.suaentrada.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class UsuarioService {
+    private final UsuarioRepository usuarioRepository;
+
+    public Usuario salvar(Usuario usuario){
+        return usuarioRepository.save(usuario);
+    }
+
+    public List<Usuario> listarTodos(){
+        return usuarioRepository.findAll();
+    }
+
+    public Usuario buscarPorId(Long codigoUsuario){
+        return usuarioRepository.findByCodigoUsuario(codigoUsuario).orElseThrow(() -> new RuntimeException("Usuario não encontrado"));
+    }
+
+    public void deletar(Long codigoUsuario){
+        usuarioRepository.delete(buscarPorId(codigoUsuario));
+    }
 }
