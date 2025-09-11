67e531aa2f5fbfd08b81ee6ae3def27561a562ac
RochaGR <gutop.rocha@gmail.com>
2025-09-11 14:59:36 -0300

    Feat: UsuarioDto criado

--- suaentrada/src/main/java/com/suaentrada/br/suaentrada/dto/UsuarioDto.java
+++ suaentrada/src/main/java/com/suaentrada/br/suaentrada/dto/UsuarioDto.java
@@ -1,4 +1,20 @@
 package com.suaentrada.br.suaentrada.dto;
 
-public class UsuarioDto {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record UsuarioDto (
+
+        @NotNull(message = "Nome precisa ser preenchido")
+        String nomeUsuario,
+
+        @NotNull(message = "Email precisa ser preenchido")
+        @Email
+        String emailUsuario,
+
+        @NotNull(message = "É necessario incluir sua senha")
+        @Size(min = 6, max = 100, message = "A senha precisa ser maior que 6 digitos")
+        String senhaUsuario
+){
 }
