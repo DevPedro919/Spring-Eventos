5a908de646a17f95955fa83bb7e248687ee018d1
pedro_hc_santana <santanapedro0105@gmail.com>
2025-09-17 14:45:22 -0300

    Implementado SecurityConfig

---
+++ suaentrada/src/main/java/com/suaentrada/br/suaentrada/configuration/SecurityConfig.java
@@ -0,0 +1,124 @@
+package com.suaentrada.br.suaentrada.configuration;
+
+import com.suaentrada.br.suaentrada.repository.UsuarioRepository;
+import com.suaentrada.br.suaentrada.util.JwtAuthFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public SecurityConfig(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, JwtAuthFilter jwtAuthFilter) throws Exception {
+        http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource())) // ADICIONAR ESTA LINHA
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/usuarios").permitAll()
+                        .requestMatchers("/roles/**").permitAll()
+                        .requestMatchers("/usuarios").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/eventos").permitAll()
+
+                        .requestMatchers("/eventos/**").hasAnyRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/eventos").hasAnyRole("USER")
+                        .requestMatchers("/inscricoes/**").hasAnyRole("USER", "ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                ;
+    }
+
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return email -> usuarioRepository.findByEmailUsuario(email)
+                .map(usuario -> {
+                    List<GrantedAuthority> authorities = usuario.getRoles().stream()
+                            .map(role -> new SimpleGrantedAuthority("ROLE_" + role.getNomeRole()))
+                            .collect(Collectors.toList());
+
+                    return new User(
+                            usuario.getEmailUsuario(),
+                            usuario.getSenhaUsuario(),
+                            authorities
+                    );
+                })
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado com email: " + email));
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
