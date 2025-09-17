a766d307457d9622d2e4b09bf89202d3b307cee9
adrian_b_oliveira <adrianbiehl2011@gmail.com>
2025-09-17 14:27:32 -0300

    AuthDto: criado response e request

---
+++ suaentrada/src/main/java/com/suaentrada/br/suaentrada/dto/AuthResponseDto.java
@@ -0,0 +1,16 @@
+package com.suaentrada.br.suaentrada.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthResponseDto {
+    private String token;
+
+}
